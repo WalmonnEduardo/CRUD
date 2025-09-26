168479be6e193d7bd641e0ca39e3f4cc3320755c
Walmonn Eduardo <130590544+WalmonnEduardo@users.noreply.github.com>
2025-09-26 15:42:33 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -5,6 +5,6 @@ define("AMB_DEV",true);
 //Configurações do banco de dados
 define("DB_HOST","127.0.0.1");
 define("DB_NAME","dbRobotica");
-define("DB_USER","root");
-define("DB_PASS","[\$mysql1111WE]");
+define("DB_USER","seu_usuario");
+define("DB_PASS","sua_senha");
 ?>
